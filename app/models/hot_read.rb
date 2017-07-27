3ff21198ed1c93004944739360ce7b97c67f8860
slague <stephanielague@gmail.com>
2017-07-27 15:26:23 -0600

    Refine index method based on spec criteria
    * TDD added criteria, test passing

--- app/models/hot_read.rb
+++ app/models/hot_read.rb
@@ -1,4 +1,12 @@
 class HotRead < ActiveRecord::Base
 
   validates :url, presence: true
+
+  def self.top_ten
+    order(read_count: :desc).limit(10)
+  end
+
+  def self.within_past_24_hrs
+    where('updated_at >= ?', 24.hours.ago)
+  end
 end
