b216268b96265a81dce6f3ca7d7ca2c2c78037ac
slague <stephanielague@gmail.com>
2017-07-27 12:22:58 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "hot_reads#index"
-  resources :hot_reads, only: [:index]
+  resources :hot_reads, only: [:index, :create, :update]
 end
