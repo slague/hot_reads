3f42400b700fa35fe5078196f9d2059fe2103709
slague <stephanielague@gmail.com>
2017-07-27 18:02:20 -0600

    Add index to api endpoints so that other app can make a get request for “hot reads” labels

--- app/controllers/api/v1/hot_reads_controller.rb
+++ app/controllers/api/v1/hot_reads_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::HotReadsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    @hot_reads = HotRead.within_past_24_hrs.top_ten
+    render status: 201, json: @hot_reads
+  end
+
   def create
     @hot_read= HotRead.find_or_initialize_by(url: params[:url])
 
